d2da18e157aca55480b51b1a03b96b8c6c828ebe
George-Wilcockson <124965296+George-Wilcockson@users.noreply.github.com>
2023-06-01 17:09:43 +0100

    Commit005 - Save class added, save method added

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -8,17 +9,35 @@ public class MainManager : MonoBehaviour
 
     public static MainManager Instance;
 
+    public Color TeamColor; // new variable declared
+
     private void Awake()
     {
-        // start of new code
         if (Instance != null)
         {
             Destroy(gameObject);
             return;
         }
-        // end of new code
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+
 }
