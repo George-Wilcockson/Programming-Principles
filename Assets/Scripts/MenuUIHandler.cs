384437b64c8d80cef428cc262527ae439e3b49ea
George-Wilcockson <124965296+George-Wilcockson@users.noreply.github.com>
2023-06-01 16:45:41 +0100

    Commit004 - Back to main menu scene flow added

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
@@ -14,7 +18,7 @@ public class MenuUIHandler : MonoBehaviour
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
-
+        MainManager.Instance.TeamColor = color;
     }
     
     private void Start()
@@ -28,4 +32,15 @@ public class MenuUIHandler : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    public void Exit()
+    {
+    #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+    #else
+        Application.Quit(); // original code to quit Unity player
+    #endif
+    }
+
+
 }
